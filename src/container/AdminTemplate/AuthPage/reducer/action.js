5ecbd0f2a28f0e4f5b1dc49b3496c63e42eaf525
quocthai99 <quocthai5101999@gmail.comn>
2022-07-04 15:08:27 +0700

    refactor-api

--- src/container/AdminTemplate/AuthPage/reducer/action.js
+++ src/container/AdminTemplate/AuthPage/reducer/action.js
@@ -1,18 +1,20 @@
 import * as ActionType from "./constant"
 import axios from "axios"
+import { api } from "../../../../util/apiUtil"
 
 export const actLogin = (user, navigate) => {
     return (dispatch) => {
         dispatch(actAuthRequest())
 
-        axios({
-            url: "https://movienew.cybersoft.edu.vn/api/QuanLyNguoiDung/DangNhap",
-            method: "POST",
-            data: user,
-            headers: {
-                TokenCybersoft: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyNSIsIkhldEhhblN0cmluZyI6IjE2LzEyLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY3MTE0ODgwMDAwMCIsIm5iZiI6MTY0MTU3NDgwMCwiZXhwIjoxNjcxMjk2NDAwfQ.cB7cdIfS0TKI1Yx_WRS-tEOt5K5yf3QJCot63SYEOHo"
-            }
-        })
+        // axios({
+        //     url: "https://movienew.cybersoft.edu.vn/api/QuanLyNguoiDung/DangNhap",
+        //     method: "POST",
+        //     data: user,
+        //     headers: {
+        //         TokenCybersoft: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyNSIsIkhldEhhblN0cmluZyI6IjE2LzEyLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY3MTE0ODgwMDAwMCIsIm5iZiI6MTY0MTU3NDgwMCwiZXhwIjoxNjcxMjk2NDAwfQ.cB7cdIfS0TKI1Yx_WRS-tEOt5K5yf3QJCot63SYEOHo"
+        //     }
+        // })
+        api.post("QuanLyNguoiDung/DangNhap", user)
             .then(result => {
                 if(result.data.content.maLoaiNguoiDung === "KhachHang") {
                     // Error
--- src/container/HomeTemplate/ListMoviePage/reducer/actions.js
+++ src/container/HomeTemplate/ListMoviePage/reducer/actions.js
@@ -1,5 +1,6 @@
 import { LIST_MOVIE_REQUEST, LIST_MOVIE_SUCCESS, LIST_MOVIE_ERROR } from "./constant"
-import axios from 'axios'
+// import axios from 'axios'
+import { api } from "../../../../util/apiUtil"
 
 
 const actListMovieRequest = () => {
@@ -26,13 +27,14 @@ export const actFetchData = () => {
     return (dispatch) => {
         dispatch(actListMovieRequest())
 
-        axios({
-            url: "https://movienew.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01",
-            method: "GET",
-            headers: {
-                "TokenCybersoft": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyNSIsIkhldEhhblN0cmluZyI6IjE2LzEyLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY3MTE0ODgwMDAwMCIsIm5iZiI6MTY0MTU3NDgwMCwiZXhwIjoxNjcxMjk2NDAwfQ.cB7cdIfS0TKI1Yx_WRS-tEOt5K5yf3QJCot63SYEOHo"
-            }
-        })
+        // axios({
+        //     url: "https://movienew.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01",
+        //     method: "GET",
+        //     headers: {
+        //         "TokenCybersoft": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyNSIsIkhldEhhblN0cmluZyI6IjE2LzEyLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY3MTE0ODgwMDAwMCIsIm5iZiI6MTY0MTU3NDgwMCwiZXhwIjoxNjcxMjk2NDAwfQ.cB7cdIfS0TKI1Yx_WRS-tEOt5K5yf3QJCot63SYEOHo"
+        //     }
+        // })
+        api.get("QuanLyPhim/LayDanhSachPhim?maNhom=GP01")
             .then(result => {
                 dispatch(actListMovieSuccess(result.data.content))
             })
